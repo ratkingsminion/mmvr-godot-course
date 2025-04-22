9b968d90ccfa63ea762609b9aff9e9e31aa75d95
Friedrich Hanisch <friedrich@ratking.de>
2025-04-22 11:55:57 +0200

    character 3d / spawn box / volumetric fog

---
+++ ss25/scripts/character_3d.gd
@@ -0,0 +1,28 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@onready var anim_tree: AnimationTree = self.find_children("*", "AnimationTree")[0]
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+	anim_tree.set("parameters/conditions/fly", not is_on_floor())
+	anim_tree.set("parameters/conditions/idle", not velocity)
+	anim_tree.set("parameters/conditions/walk", velocity != Vector3.ZERO)
